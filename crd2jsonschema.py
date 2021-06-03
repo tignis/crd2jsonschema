b1752c4dc844fbeaba156fbd2e2e36df369cb431
Alan J Castonguay <alan@tignis.com>
2021-06-03 15:52:06 -0400

    Add support for CRDs with multiple versions

--- crd2jsonschema.py
+++ crd2jsonschema.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 
 import yaml  # https://pyyaml.org/wiki/PyYAMLDocumentation
+from deepdiff import DeepDiff  # https://pypi.org/project/deepdiff/
 import json
 import os
 import re
+from pprint import pprint
+import shutil
 
 
-DNS_1123_REGEX = re.compile('[a-z0-9]([-a-z0-9]*[a-z0-9])?')
+dns_1123_regex = re.compile('[a-z0-9]([-a-z0-9]*[a-z0-9])?')
 
 
-def generate_filename(crd_dictionary):
+def generate_filename(crd_dictionary, version_name=None):
     """
     Given a dictionary loaded from a CRD yaml, extract the resource group, version, kind. Return a filename matching kubeval's expectations.
     https://github.com/instrumenta/kubeval/blob/8c40ed5d2110c5dd7fe45d6a429f16170c9910d1/kubeval/kubeval.go#L40
@@ -49,77 +52,125 @@ def generate_filename(crd_dictionary):
         kind = crd_dictionary['spec']['names']['singular'].lower()
     except KeyError:
         kind = crd_dictionary['spec']['names']['kind'].lower()
+
     group_parts = crd_dictionary['spec']['group'].split('/')[0].split('.')
-    version_part = crd_dictionary['spec']['version']
+
+    version_part = version_name or crd_dictionary['spec']['version']
 
     kind_suffix = group_parts[0].lower()
-    if len(group_parts)>2:
-        kind_suffix += "-" + group_parts[1].lower()
+    # As of kubeval 0.15, this extra piece appears to be undesirable.
+    #if len(group_parts)>2:
+    #    kind_suffix += "-" + group_parts[1].lower()
 
-    if not DNS_1123_REGEX.fullmatch(kind):
+    if not dns_1123_regex.fullmatch(kind):
         raise ValueError('Invalid kind', kind)
-    if not DNS_1123_REGEX.fullmatch(kind_suffix):
+    if not dns_1123_regex.fullmatch(kind_suffix):
         raise ValueError('Invalid group', kind_suffix)
-    if not DNS_1123_REGEX.fullmatch(version_part):
+    if not dns_1123_regex.fullmatch(version_part):
         raise ValueError('Invalid version', version_part)
 
-    return '%s-%s-%s.json' % (
+    r = '%s-%s-%s.json' % (
         kind,
         kind_suffix,
         version_part
     )
+    print("Generated name", r)
+    return r
 
 
 def process_yaml_file(yaml_in, output_directory):
+    print("Reading", yaml_in)
     yaml_object = yaml.safe_load_all(yaml_in) # yaml_object will be a list or a dict
-
-    wrote_filenames = []
 
     for document in yaml_object:
         if document is None: continue  # skip empty yaml document
 
-        filename = generate_filename(document)
-        schema_part = document['spec']['validation']['openAPIV3Schema']
+        if document['kind'] != 'CustomResourceDefinition':
+            continue
 
-        # Patch the schema with kubernetes extensions
-        schema_part['x-kubernetes-group-version-kind'] = [{
-            'kind': [document['spec']['names']['kind']],
-            'version': document['spec']['version'],
-            'group': document['spec']['group'],
-        }]
+        # A CRD may list multiple versions, or just one. Which is it?
+        # Assume multiple.
+        versions_chunks = document['spec']['versions']
 
-        # This is json-schema, even though the CRD didn't state that explicitly.
-        schema_part['$schema'] = 'http://json-schema.org/schema#'
+        print("Found versions", [x['name'] for x in versions_chunks])
 
+        for version_chunk in versions_chunks:
+            try:
+                schema_part = version_chunk['schema']['openAPIV3Schema']
+            except:
+                continue
+
+            filename = generate_filename(document, version_chunk['name'])
+
+            # Patch the schema with kubernetes extensions
+            schema_part['x-kubernetes-group-version-kind'] = [{
+                'kind': [document['spec']['names']['kind']],
+                'version': version_chunk['name'],
+                'group': document['spec']['group'],
+            }]
+
+            # This is json-schema, even though the CRD didn't state that explicitly.
+            schema_part['$schema'] = 'http://json-schema.org/schema#'
+
+            emit_file(output_directory, filename, schema_part)
+
+        # One mega version, not in the list of multiple versions
+
+        if 'version' in document['spec']:
+            filename = generate_filename(document)
+            try:
+                schema_part = document['spec']['validation']['openAPIV3Schema']
+            except KeyError:
+                print("No schema found in spec.validation.openAPIV3Schema")
+                schema_part = {
+                    "type": "object",
+                    "additionalProperties": True,
+                }
+
+            # Patch the schema with kubernetes extensions
+            schema_part['x-kubernetes-group-version-kind'] = [{
+                'kind': [document['spec']['names']['kind']],
+                'version': document['spec']['version'],
+                'group': document['spec']['group'],
+            }]
+
+            # This is json-schema, even though the CRD didn't state that explicitly.
+            schema_part['$schema'] = 'http://json-schema.org/schema#'
+
+            emit_file(output_directory, filename, schema_part)
+
+
+def emit_file(output_directory, filename, schema_part):
+    try:
         with open(os.path.join(output_directory, filename), 'x') as json_out:
             json.dump(schema_part, json_out, indent=2)
+        print("Wrote %s" % filename)
 
-        wrote_filenames.append(filename)
+    except FileExistsError as e:
+        
+        with open(os.path.join(output_directory, filename), 'r') as json_comparison:
+            existing_file_content = json.load(json_comparison)
 
-    return wrote_filenames
+        ddiff = DeepDiff(schema_part, existing_file_content, ignore_order=True)
 
+        if ddiff:
+            print("Existing %s already exists, with different content. Delete it and recreate?" % filename)
+            #pprint(ddiff, indent=1, width=min([80, shutil.get_terminal_size().columns]), compact=False)
+        else:
+            print("Existing %s already correct" % filename)        
+    
 
-def main():
+if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser(description=(
         'Given a list of yaml files containing Kubernetes Custom Resource Definitions (CRDs), '
         'extract the embedded openAPIV3Schema into a standalone file usable by http://kubeval.instrumenta.dev'
     ))
-    parser.add_argument('--output-directory', default=os.getcwd(), help="write json-schema files to this directory")
-    parser.add_argument('-q', target='quiet', action='store_true', help="emit less output")
-    parser.add_argument('yaml_files', metavar='crd.yaml', nargs='+', type=argparse.FileType('r'))
-
+    parser.add_argument('--output-directory', default=os.getcwd(), help="Write json-schema files to this directory")
+    parser.add_argument('yaml_files', metavar='yaml file', nargs='+', type=argparse.FileType('r'))
 
     args = parser.parse_args()
 
     for yaml_file_handle in args.yaml_files:
-        r = process_yaml_file(yaml_file_handle, args.output_directory)
-
-        if not args.quiet:
-            for fn in r:
-                print(fn)
-
-
-if __name__ == '__main__':
-    main()
+        process_yaml_file(yaml_file_handle, args.output_directory)
